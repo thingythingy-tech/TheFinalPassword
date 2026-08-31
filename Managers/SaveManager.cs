56ffd8575e84457baf9cdcfb40b2a95de45df0d3
Jamie Harris <jamienewlaptop@gmail.com>
2026-08-31 12:19:48 +0100

    halfway through phase 7

---
+++ Managers/SaveManager.cs
@@ -0,0 +1,21 @@
+﻿using TheFinalPassword.Models;
+
+namespace TheFinalPassword.Managers;
+using System.Text.Json;
+
+public class SaveManager
+{
+    private string savePath = "savegame.json";
+
+    public void SaveGame(SaveData data)
+    {
+        string json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true});
+        File.WriteAllText(savePath, json);
+    }
+    public SaveData LoadGame()
+    {
+        if (!SaveExists()) return null;
+        string json = File.ReadAllText(savePath);
+        return JsonSerializer.Deserialize<SaveData>(json);
+    }
+}
---
+++ Models/SaveData.cs
@@ -0,0 +1,10 @@
+﻿namespace TheFinalPassword.Models;
+
+public class SaveData
+{
+    public string PlayerName { get; set; }
+    public List<string> Inventory { get; set; }
+    public int Progress { get; set; }
+    public int Reputation { get; set; }
+    public bool AuthenicationStatus { get; set; }
+}
